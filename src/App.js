8e1071c75dda35b4553818e0e2b53b112fddb8be
Min Htut <minhtutzaw16@gmail.com>
2026-01-04 00:18:47 +0630

    Use State Hook

--- src/App.js
+++ src/App.js
@@ -1,12 +1,20 @@
 
+import { useState } from 'react';
 import './App.css';
 import Img from './assets/img.png'
 
 function App() {
+  let [world,setworld] = useState("World")
+  let change =()=>{
+    setworld("React")
+  }
+
+
   return (
     <>
-    <h1>Hello world</h1>
-    <img src={Img} alt="" srcset="school uniform" />
+      <h1>Hello {world}</h1>
+      <button onClick={change}>Changes</button>
+
     </>
   );
 }
