4f2fd358f25ebb0c5c27c1856c8ca6b99c4462a1
Min Htut <minhtutzaw16@gmail.com>
2026-01-04 00:55:53 +0630

    Looping array map

--- src/App.js
+++ src/App.js
@@ -4,16 +4,31 @@ import './App.css';
 import Img from './assets/img.png'
 
 function App() {
-  let [world,setworld] = useState("World")
-  let change =()=>{
+  let [world, setworld] = useState("World")
+  let change = () => {
     setworld("React")
-  }
+  };
+
+  let [blogs, setBlogs] = useState([
+    { 'id': 1, 'author': 'leo' },
+    { 'id': 2, 'author': 'leon' }
+
+
+  ]);
 
 
   return (
     <>
       <h1>Hello {world}</h1>
       <button onClick={change}>Changes</button>
+
+      <div>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+            {blog.id} - {blog.author}
+          </li>
+        ))}
+      </div>
 
     </>
   );
