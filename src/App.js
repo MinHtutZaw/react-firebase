adb45059daef1dd27de0278a2347dc3ffbd61927
Min Htut <minhtutzaw16@gmail.com>
2026-01-06 00:29:37 +0630

    implement blog list with delete feature

--- src/App.js
+++ src/App.js
@@ -1,23 +1,46 @@
-import React, { useState } from 'react'
 
-export default function App() {
- let [count,setCount] =useState(0)
- let increment=()=>{
-  setCount((prevState)=>prevState+1)
- }
-  let decrement=()=>{
-  setCount((prevState)=>prevState-1)
- }
+import { useState } from 'react';
+import './App.css';
+import Img from './assets/img.png'
+
+function App() {
+  let [world, setworld] = useState("World")
+  let change = () => {
+    setworld("React")
+  };
+
+  let [blogs, setBlogs] = useState([
+    { 'id': 1, 'author': 'leo' },
+    { 'id': 2, 'author': 'leon' }
+
+
+  ]);
+  let deleteBlog =(id)=>{
+    setBlogs((prevState)=>prevState.filter(blog=>blog.id != id))
+
+  }
+
 
   return (
     <>
-      <h1>Counter task</h1>
-      <h2>Count: {count}</h2>
-      <button onClick={increment}>Increment</button>
-       <button onClick={decrement}>Decrement</button>
-      
+      <h1>Hello {world}</h1>
+      <button onClick={change}>Changes</button>
+
+      <div>
+        <h1>Blogs</h1>
+        {blogs.map((blog) => (
+          <li key={blog.id}>
+            {blog.id} - {blog.author}
+            <button onClick={()=>deleteBlog(blog.id)}>Delete button </button>
+          </li>
+        ))}
+      </div>
+
     </>
-
-
-  )
+  );
 }
+
+export default App;
+
+
+
